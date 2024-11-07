cea25054c41fa001f0499d88fb2612bc91172964
josh-W42 <wilson.jd022@gmail.com>
2024-11-06 22:29:19 -0800

    Added CORS for flask and allowed front-end to send requests as an origin, cleaned up untested logic and tested image upload and analysis feature

--- server/main.py
+++ server/main.py
@@ -1,13 +1,21 @@
 import os.path
+import time
 
 from flask import Flask, request, jsonify
-from utils import is_file_allowed, extract_colors
+from utils import extract_colors, is_file_allowed, remove_image
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/temp'
+UPLOAD_FOLDER = './temp'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+CORS(app, resources={
+    r'/api/*': {
+        "origins": ["http://localhost:5173"]
+    }
+})
 
 
 @app.route('/')
@@ -15,7 +23,7 @@ def hello_world():
     return '<h1>Hello World</h1>'
 
 
-@app.route('/images/extract', methods=['POST'])
+@app.route('/api/images/extract', methods=['POST'])
 def upload_image():
     if 'image' not in request.files:
         return jsonify({'error': 'No image file provided'}), 400
@@ -28,19 +36,23 @@ def upload_image():
     if not is_file_allowed(image.filename):
         return jsonify({'error': 'Invalid file type uploaded.'}), 400
 
-    filename = secure_filename(image.filename)
+    filename = f'{time.time()}-{secure_filename(image.filename)}'
     path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
     try:
         image.save(path)
     except Exception as e:
+        remove_image(path)
         return jsonify({'error': 'Failed to save image', 'message': str(e)}), 500
 
     try:
         results = extract_colors(path, 5)
-        return jsonify({'message': 'Image uploaded successfully', 'data': results}), 200
+        results = results.tolist()
+        remove_image(path)
+        return jsonify({'message': 'Successfully extracted color data', 'data': results}), 200
     except Exception as e:
-        return jsonify({'error': 'Failed to extract color from image', 'message': str(e)}), 500
+        remove_image(path)
+        return jsonify({'error': 'Failed to extract color data from image', 'message': str(e)}), 500
 
 
 if __name__ == '__main__':
