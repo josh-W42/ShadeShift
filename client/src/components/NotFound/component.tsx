a6ce84e315b481a5548b1d964c3bac51c2379421
josh-W42 <wilson.jd022@gmail.com>
2024-11-07 15:58:10 -0800

    Added a generic 404 not found component

---
+++ client/src/components/NotFound/component.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from 'react';
+
+export const NotFound: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Page Not Found</p>
+    </div>
+  );
+};
