60511f0c1672e4e93edacbed194f9a8c102e3daf
josh-W42 <wilson.jd022@gmail.com>
2024-11-06 22:29:51 -0800

    Created a basic form to test image upload and processing feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
 function App() {
   return (
     <>
-      <form>
-        <input type="file" />
+      <form
+        action="http://localhost:8000/api/images/extract"
+        method="post"
+        encType="multipart/form-data"
+      >
+        <input name="image" type="file" accept=".jpg,.jpeg,.png" />
+        <button type="submit">Submit Photo</button>
       </form>
     </>
   );
