d3115b99fd4b0ffa27de1a123e45298327eec2b3
josh-W42 <wilson.jd022@gmail.com>
2024-11-07 22:01:44 -0800

    Added a types file specifically for palette related code

---
+++ client/src/utils/palettes/types.ts
@@ -0,0 +1,14 @@
+export const enum ColorHarmonyType {
+  analogous = 'analogous',
+  triadic = 'triadic',
+  tetradic = 'tetradic',
+  complementary = 'complementary',
+  splitComplementary = 'splitComplementary',
+}
+
+export const enum GenerationType {
+  random = 'random',
+  scientific = 'scientific',
+  discovery = 'discovery',
+  hueShift = 'hueShift',
+}
