5d37b098c40dfa0aff4e02bb7b0855e320c1fbe0
josh-W42 <wilson.jd022@gmail.com>
2024-11-08 18:48:12 -0800

    fixed bug in DEFAULT_THEME constant where I was incorrectly initalizing a Color class instance

--- client/src/utils/theme/types.ts
+++ client/src/utils/theme/types.ts
@@ -13,8 +13,8 @@ export const DEFAULT_THEME: Theme = {
   primary: '#1BBA8A',
   secondary: '#7EA1FF',
   tertiary: '#F18865',
-  lighter: Color({ hex: '#1BBA8A', luminosity: 98, chroma: 10 }).hex(),
-  darker: Color({ hex: '#1BBA8A', luminosity: 10, chroma: 20 }).hex(),
+  lighter: Color('#1BBA8A').l(98).chroma(10).hex(),
+  darker: Color('#1BBA8A').l(10).chroma(20).hex(),
 };
 
 export const ThemeContext = createContext(DEFAULT_THEME);
