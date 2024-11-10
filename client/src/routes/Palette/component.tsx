022a1d7872297acd35d7a618d18ddd10afb05c91
josh-W42 <wilson.jd022@gmail.com>
2024-11-10 10:27:27 -0800

    Updated replaced palette page component logic with the ColorBlock component, Applied CSS styles for shade mode

--- client/src/routes/Palette/component.tsx
+++ client/src/routes/Palette/component.tsx
@@ -1,7 +1,7 @@
 import Color from 'color';
 import { FunctionComponent } from 'react';
 import './palette.css';
-import { copyToClipBoard } from '../../utils';
+import { ColorBlock } from '../../components';
 
 interface Props {
   colors: Color<string>[];
@@ -9,22 +9,7 @@ interface Props {
 
 export const PalettePageComponent: FunctionComponent<Props> = ({ colors }) => {
   const getColors = () => {
-    return colors.map((color, i) => (
-      <div
-        className={`colorBlock ${color.isDark() ? 'darkBlock' : 'lightBlock'}`}
-        style={{
-          backgroundColor: color.hexa(),
-        }}
-        key={color.hex() + i}
-      >
-        <button onClick={() => copyToClipBoard(color.hex())}>Copy</button>
-        <button className="blockHexCode">{color.hex().slice(1)}</button>
-        <button className="blockSecondaryInfo">
-          {`rgb(${color.red()}, ${color.green()}, ${color.blue()})`}
-        </button>
-      </div>
-    ));
+    return colors.map((color) => <ColorBlock color={color} />);
   };
-
   return <div id="colorContainer">{getColors()}</div>;
 };
