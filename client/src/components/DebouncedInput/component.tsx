7ed9ce43683df23019e59f08d975d05d4635ac0b
josh-W42 <wilson.jd022@gmail.com>
2024-11-12 20:25:10 -0800

    Added a debounce input component that can be used to slow down rapidly changing inputs, like color pickers

---
+++ client/src/components/DebouncedInput/component.tsx
@@ -0,0 +1,22 @@
+import { Input, InputProps } from '@mui/material';
+import { ChangeEvent, useRef, useState } from 'react';
+
+type DebounceProps = {
+  handleDebounce: (value: string) => void;
+  debounceTimeout: number;
+};
+
+export function DebounceInput(props: InputProps & DebounceProps) {
+  const { handleDebounce, debounceTimeout, ...other } = props;
+
+  const timerRef = useRef<ReturnType<typeof setTimeout> | undefined>(undefined);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    clearTimeout(timerRef.current);
+    timerRef.current = setTimeout(() => {
+      handleDebounce(event.target.value);
+    }, debounceTimeout);
+  };
+
+  return <Input {...other} onChange={handleChange} />;
+}
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,3 +2,4 @@ export * from './NotFound';
 export * from './TopNavBar';
 export * from './ColorBlock';
 export * from './PaletteGenModal';
+export * from './DebouncedInput';
