b13f3e843e0ad7a35d1a3d0664f743adc819bfbd
josh-W42 <wilson.jd022@gmail.com>
2024-11-17 18:23:33 -0800

    Modified the getColorFormat function, formats should now be correctly displayed

--- client/src/utils/colors/getColorFormat.ts
+++ client/src/utils/colors/getColorFormat.ts
@@ -2,47 +2,43 @@ import Color from 'color';
 import { ColorFormats } from './types';
 
 /**
- * Converts a color to a string representation in a given color format.
+ * Returns a string representation of the given color in the specified format.
  *
- * @param format The color format to convert to. Can be one of:
- *   - 'rgb'
- *   - 'name'
- *   - 'cmyk'
- *   - 'hsl'
- *   - 'hsv'
- *   - 'lch'
- *   - 'hwb'
- * @param color The color to convert.
- *
- * @returns A string representation of the color in the given format.
+ * @param format - The desired color format.
+ * @param color - The color to format.
+ * @returns A string representation of the color in the specified format.
  */
 export const getColorFormat = (format: ColorFormats, color: Color) => {
   switch (format) {
     case ColorFormats.rgb:
-      return color.toString().toUpperCase();
+      return `rgb(${Math.round(color.red())}, ${Math.round(
+        color.green()
+      )}, ${Math.round(color.blue())})`;
     case ColorFormats.name:
       return `${color.keyword()[0].toUpperCase()}${color.keyword().slice(1)}`;
     case ColorFormats.cmyk:
-      return `CMYK(${Math.round(color.cyan())}, ${Math.round(
+      return `cmyk(${Math.round(color.cyan())}%, ${Math.round(
         color.magenta()
-      )}, ${Math.round(color.yellow())}, ${Math.round(color.black())})
+      )}%, ${Math.round(color.yellow())}%, ${Math.round(color.black())}%)
       `;
     case ColorFormats.hsl:
-      return `HSL: (${Math.round(color.hue())} ${Math.round(
+      return `hsl(${Math.round(color.hue())}, ${Math.round(
         color.saturationl()
-      )}, ${Math.round(color.l())})`;
+      )}%, ${Math.round(color.l())}%)`;
     case ColorFormats.hsv:
-      return `HSV: (${Math.round(color.hue())}, ${Math.round(
+      return `hsv(${Math.round(color.hue())}, ${Math.round(
         color.saturationv()
-      )}, ${Math.round(color.value())})`;
+      )}%, ${Math.round(color.value())}%)`;
     case ColorFormats.lch:
-      return `LCH: (${Math.round(color.l())}, ${Math.round(
+      return `lch: ${Math.round(color.l())}, ${Math.round(
         color.chroma()
-      )}, ${Math.round(color.hue())})`;
+      )}, ${Math.round(color.hue())}`;
     case ColorFormats.hwb:
-      return `HWB: (${Math.round(color.hue())}, ${Math.round(
+      return `hwb(${Math.round(color.hue())}, ${Math.round(
         color.white()
-      )}, ${Math.round(color.black())})`;
+      )}%, ${Math.round(color.black())}%)`;
+    case ColorFormats.hex:
+      return color.hex();
     default:
       return color.toString();
   }
