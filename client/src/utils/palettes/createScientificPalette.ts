287218715738f7b13eaa38be4ab73c8d59bab433
josh-W42 <wilson.jd022@gmail.com>
2024-11-25 13:59:26 -0800

    Added a comment about tetradic generation

--- client/src/utils/palettes/createScientificPalette.ts
+++ client/src/utils/palettes/createScientificPalette.ts
@@ -1,6 +1,17 @@
 import Color from 'color';
 import { adjustHue } from './adjustHue';
-import { targetHueSteps } from './types';
+import { ColorHarmonyType, targetHueSteps } from './types';
+
+/**
+ * TODO:
+ *
+ * Tetradic is not exactly correct.
+ *
+ * At the present time, tetradic selects colors that are positioned 90 degrees
+ * from the base color, and while this is a tetradic color scheme, not all
+ * tetradic color schemes are square, they can be rectangles. As long as there are
+ * 2 complementary sets of colors then that too can be tetradic.
+ */
 
 /**
  * Generates a scientific palette based on the input color.
@@ -12,7 +23,7 @@ import { targetHueSteps } from './types';
  * and values are arrays of Colors that belong to the respective harmony.
  */
 export const createScientificPalette = (color: Color) => {
-  const result = new Map<string, Color[]>();
+  const result = new Map<ColorHarmonyType, Color[]>();
 
   for (const type of targetHueSteps.keys()) {
     result.set(
