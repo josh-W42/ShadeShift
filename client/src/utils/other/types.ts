a9adc76f1c0d1e980f3e582ec39c4ea7b955ed32
josh-W42 <wilson.jd022@gmail.com>
2024-11-27 12:47:55 -0800

    Added removed a console log in the history class, added example for env, updated git ignore to include the build folder and a firebase generated file; client now pokes the server in order to spin it up before use

--- client/src/utils/other/types.ts
+++ client/src/utils/other/types.ts
@@ -1,1 +1,3 @@
-export const BASE_SERVER_URL = 'https://shadeshift-h92m.onrender.com/';
+export const BASE_SERVER_URL =
+  import.meta.env.VITE_DEVELOPMENT_SERVER_URL ||
+  'https://shadeshift-h92m.onrender.com/api';
