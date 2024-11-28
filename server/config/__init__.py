e873ce9f0718338a9970b14d30b6876a65936fd3
josh-W42 <wilson.jd022@gmail.com>
2024-11-27 16:18:06 -0800

    Updated the Session cookie settings in the app config

--- server/config/__init__.py
+++ server/config/__init__.py
@@ -38,7 +38,9 @@ CORS(
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(base_dir, 'database.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SESSION_COOKIE_SAMESITE"] = 'Lax'
+app.config["SESSION_COOKIE_SAMESITE"] = None
+app.config["SESSION_COOKIE_HTTPONLY"] = True
+app.config["SESSION_COOKIE_SECURE"] = True
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 db.init_app(app)
